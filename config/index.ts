83c5d73d7f42eab026aa0131d1e104531a5b840d
kit <kit@daangn.com>
2022-07-24 22:28:58 +0900

    fix: image map

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ import ghostsproject from './ghostsproject.json'
 import supernormalbyzipcy from './supernormalbyzipcy.json'
 
 const images = {
-  shyghostSquad,
-  ghostsproject,
-  supernormalbyzipcy,
+  '0x487b068009c7094fe8dc9452bc804f1129709288': shyghostSquad,
+  '0xdf3407636bbf3a015a8e48a079ef7ba49e687fd3': ghostsproject,
+  '0xd532b88607b1877fe20c181cba2550e3bbd6b31c': supernormalbyzipcy,
 }
 
 export default images
